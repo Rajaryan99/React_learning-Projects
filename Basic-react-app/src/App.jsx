62f27ec56d1773fd8f8e34e97398b03f64c07b28
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-27 11:43:05 +0530

    1st basic react app

---
+++ Basic-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello world!</p>
+      <button>click me</button>
+    </>
+  )
+}
+
+export default App
--- vite-project/src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Coffee aur React with Vite | Raj Aryan</h1>
-  );
-}
-
-export default App;
