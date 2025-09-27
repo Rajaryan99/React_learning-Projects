5e6102bfbd39f0dba2a4a764ce9aef2c4dbb8709
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-27 11:54:55 +0530

    1st component

--- Basic-react-app/src/App.jsx
+++ Basic-react-app/src/App.jsx
@@ -1,10 +1,15 @@
 
 import './App.css'
 
+function Title() {
+  return <h1>I am title the 1st component..</h1>
+}
+
 function App() {
 
   return (
     <>
+      <Title />
       <p>Hello world!</p>
       <button>click me</button>
     </>
