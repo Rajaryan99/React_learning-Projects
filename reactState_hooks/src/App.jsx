6bbf3c6a61b68d4e95e686d5a297d9129edea8e3
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-07 22:19:46 +0530

    made a lottery component dynamic

--- reactState_hooks/src/App.jsx
+++ reactState_hooks/src/App.jsx
@@ -1,18 +1,14 @@
 
 import './App.css'
-import Counter from './Counter'
-import LikeButton from './LikeButton'
-import LudoBorad from './LudoBorad'
-import TodoList from './TodoList'
 import Lottery from './lottery'
-import Ticket from './Ticket'
+
 
 function App() {
   
 
   return (
     <>
-     <Ticket  ticket={[0, 1, 2]} />
+     <Lottery n={3} winningSum={15}/>
     </>
   )
 }
--- reactState_hooks/src/lottery.jsx
+++ reactState_hooks/src/lottery.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react"
 import './lottery.css'
 import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
 
-export default function Lottery() {
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning  = sum(ticket) === 15;
+export default function Lottery({n, winningSum} ) {
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning  = sum(ticket) === winningSum;
     let buyTicket  = () => {
-        setTicket(genTicket(3))
+        setTicket(genTicket(n))
     }
     return (
         <div>
             <h1>Lottery Game!!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
+          <Ticket ticket={ticket}/>
             <br />
         {isWinning ? <h3>Congratulation.. You Won!</h3> : <button onClick={buyTicket}>Buy New Ticket</button> }
         </div>
