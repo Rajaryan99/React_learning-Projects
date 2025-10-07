d63cbe30fbf22b21bba933ab9ad74b7bd27822b5
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-07 10:40:19 +0530

    ternimary operator

--- reactState_hooks/src/lottery.jsx
+++ reactState_hooks/src/lottery.jsx
@@ -5,6 +5,9 @@ import { genTicket, sum } from "./helper";
 export default function Lottery() {
     let [ticket, setTicket] = useState(genTicket(3));
     let isWinning  = sum(ticket) === 15;
+    let buyTicket  = () => {
+        setTicket(genTicket(3))
+    }
     return (
         <div>
             <h1>Lottery Game!!</h1>
@@ -13,7 +16,8 @@ export default function Lottery() {
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
-            <h3>{isWinning && 'Congratulations you won!!'}</h3>
+            <br />
+        {isWinning ? <h3>Congratulation.. You Won!</h3> : <button onClick={buyTicket}>Buy New Ticket</button> }
         </div>
     )
 }
