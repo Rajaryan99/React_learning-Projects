60411c14db3da81c969013d3e30389b8cae1c2cd
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-09 20:04:22 +0530

    handing events

--- reactState_hooks/src/Form.jsx
+++ reactState_hooks/src/Form.jsx
@@ -2,17 +2,38 @@ import { useState } from "react"
 
 export default function Form() {
 
-    let [username, setUsername] = useState('');
 
-    let handleNameChange = (e) => {
-        setUsername(e.target.value)
+    let [fieldData, setfieldData] = useState({
+        fullname: '',
+        username: '',
+    })
+
+    let handleInputChange = (e) => {
+        console.log(e.target.value);
     }
+
+    // let [username, setUsername] = useState('');
+    // let [fullname, setfullname] = useState('');
+
+    // let handleNameChange = (e) => {
+    //     setUsername(e.target.value)
+    // }
+    // let handleusername = (e) => {
+    //     setfullname(e.target.value)
+    // }
 
     return(
         <form action="">
-            
-            <input type="text" placeholder="Enter Your username" value={username} onChange={handleNameChange} />
+            <label htmlFor="fullname"> Full Name</label>
+            <input type="text" id="fullname" name="fullname" placeholder="Enter Your fullname" value={fieldData.fullname} onChange={handleInputChange} />
+             <button>submit</button>
+<br />
+              <form action="">
+            <label htmlFor="username"> Username</label>
+            <input type="text" id="Username" name="username" placeholder="Enter Your username" value={fieldData.username} onChange={handleInputChange} />
              <button>submit</button>
         </form>
+        </form>
+        
     )
 }
