84d8c36d5fe1979babe3541a3af02603e45b7d6c
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-10 21:01:16 +0530

    added useState

--- reactState_hooks/src/CommentForm.jsx
+++ reactState_hooks/src/CommentForm.jsx
@@ -1,21 +1,38 @@
+import { useState } from 'react'
 import './form.css'
 
 export default function CommentForm() {
+
+    let [formData, setformData] = useState({
+        username: '',
+        remarks: '',
+        rating: 5
+    })
+
+    let handleInputChange = (e) => {
+        setformData((currData) => {
+            return {...currData}
+        })
+    }
+
     return (
         <div>
             <h1>Give a comment!</h1>
             <form action="">
-                <input type="text" placeholder='Username' />
+                <input type="text" placeholder='Username' value={formData.username} />
                 <br />
                 <textarea name="" id="" 
                 rows={10}
                  cols={40}
                  placeholder='Write  a comment!'
+                 value={formData.remarks}
                  >
             
                  </textarea>
+                 <br />
+                 <input type="number" placeholder='Rating' min={1} max={5} value={formData.rating} />
             <br /><br />
-                <button>Submit</button>
+                <button>Add Comment</button>
             </form>
         </div>
     )
