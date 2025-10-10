0d85baccc6b7bd0bad95fa9dcb8755aef3afd43b
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-10 20:39:22 +0530

    creating a comment box using react form

--- reactState_hooks/src/App.jsx
+++ reactState_hooks/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Form2 from './Form2'
+import CommentForm from './CommentForm'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-     <Form2/>
+     <CommentForm/>
     </>
   )
 }
--- reactState_hooks/src/CommentForm.jsx
+++ reactState_hooks/src/CommentForm.jsx
@@ -1,5 +1,22 @@
 import './form.css'
 
 export default function CommentForm() {
-
+    return (
+        <div>
+            <h1>Give a comment!</h1>
+            <form action="">
+                <input type="text" placeholder='Username' />
+                <br />
+                <textarea name="" id="" 
+                rows={10}
+                 cols={40}
+                 placeholder='Write  a comment!'
+                 >
+            
+                 </textarea>
+            <br /><br />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
