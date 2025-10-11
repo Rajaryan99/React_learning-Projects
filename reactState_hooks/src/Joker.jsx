f4705a34f7ee02cd8367c8bfb0d0aa9510cabf0c
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-11 20:39:03 +0530

    fetching data form API

--- reactState_hooks/src/Joker.jsx
+++ reactState_hooks/src/Joker.jsx
@@ -1,7 +1,17 @@
 export default function Joker(){
+    
+    const URL = 'https://official-joke-api.appspot.com/random_joke'
+
+    const getJoke =  async ()  => {
+       let res =  await fetch(URL);
+       let jsonRes = await res.json();
+       console.log(jsonRes)
+    }
     return (
         <div>
             <h2>Joker!</h2>
+            <br />
+            <button onClick={getJoke}>Get New Joke</button>
         </div>
     )
 }
