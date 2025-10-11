8dd158309ea3ad54df72093d16216378c0cc8493
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-11 20:49:15 +0530

    created a random joke page

--- reactState_hooks/src/Joker.jsx
+++ reactState_hooks/src/Joker.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
+
 export default function Joker(){
+    let [joke, setjoke] = useState({});
     
     const URL = 'https://official-joke-api.appspot.com/random_joke'
 
     const getJoke =  async ()  => {
        let res =  await fetch(URL);
        let jsonRes = await res.json();
-       console.log(jsonRes)
+       console.log(jsonRes);
+       setjoke({ setup: jsonRes.setup, punchline: jsonRes.punchline});
     }
     return (
         <div>
-            <h2>Joker!</h2>
+            <h1>Joker!</h1>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
+
             <br />
             <button onClick={getJoke}>Get New Joke</button>
         </div>
