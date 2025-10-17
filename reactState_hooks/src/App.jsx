cfe1fadb48007bc5eebab2cfe94eb59a8e246199
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-17 22:23:26 +0530

    use satet

--- reactState_hooks/src/App.jsx
+++ reactState_hooks/src/App.jsx
@@ -1,12 +1,14 @@
 
+import { useState } from 'react';
 import './App.css'
 import Joker from './Joker'
 
 
 function App() {
+  let [count, setcount] = useState(0)
 
   let addValue = () => {
-    console.log('button was Clicked')
+    setcount(count + 1);
   }
   
 let counter = 5;
@@ -14,7 +16,7 @@ let counter = 5;
     <>
      <h1>React and Hooks</h1>
 
-    <h3>Counter value: {counter}</h3>
+    <h3>Counter value: {count}</h3>
 
     <button onClick={addValue}>Add Value</button> &nbsp; &nbsp;
     <button>Remove Value</button>
