2a9ee910923adcc824f595d541d450f5e00157bd
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-17 22:18:30 +0530

    counter value

--- reactState_hooks/src/App.jsx
+++ reactState_hooks/src/App.jsx
@@ -4,15 +4,20 @@ import Joker from './Joker'
 
 
 function App() {
-  
 
+  let addValue = () => {
+    console.log('button was Clicked')
+  }
+  
+let counter = 5;
   return (
     <>
      <h1>React and Hooks</h1>
 
-    <h3>Counter value: 5</h3>
+    <h3>Counter value: {counter}</h3>
 
-    
+    <button onClick={addValue}>Add Value</button> &nbsp; &nbsp;
+    <button>Remove Value</button>
     </>
   )
 }
